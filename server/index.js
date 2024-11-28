9e4ee1eecb62e6f24635a0828b50205545ea275d
Vignesh D <vignesh.d9123@gmail.com>
2024-11-28 19:21:35 +0530

    change bodyParser config

--- server/index.js
+++ server/index.js
@@ -19,13 +19,8 @@ app.use(cors(
     }
 ));
 app.use(bodyParser.json({limit: '100mb'}));
-app.use(express.json(
-
-    {
-        limit: '100mb'
-    }
-));
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true}))
+app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
