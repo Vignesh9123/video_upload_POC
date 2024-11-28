dbc05fa19b570798784dfb899fcd39a2824cb6f8
Vignesh D <vignesh.d9123@gmail.com>
2024-11-28 19:06:59 +0530

    allow cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ import { Folder } from "./models/folders.model.js";
 import { File } from "./models/files.model.js";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use('/uploads', express.static('uploads'));
