0d17cbe521c3582be22be1b0a233a01a0830722b
Vignesh D <vignesh.d9123@gmail.com>
2024-11-28 19:12:10 +0530

    extend payload limit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,12 @@ app.use(cors(
         allowedHeaders: ['Content-Type', 'Authorization']
     }
 ));
-app.use(express.json());
+app.use(express.json(
+
+    {
+        limit: '50mb'
+    }
+));
 app.use(express.urlencoded({extended: true}))
 app.use('/uploads', express.static('uploads'));
 cloudinary.config({ 
