5ea41e82c2dadf4a69108b56252c70000afb9628
Vignesh D <vignesh.d9123@gmail.com>
2024-11-29 18:54:56 +0530

    add frontend uris

--- server/constants.js
+++ server/constants.js
@@ -1,1 +1,1 @@
-export const frontendURL = 'http://localhost:5173';
+export const frontendURL = ['http://localhost:5173', 'https://virtual-class-sc.vercel.app', 'https://video-upload-poc.vercel.app', 'https://login-page-sc-blush.vercel.app'];
